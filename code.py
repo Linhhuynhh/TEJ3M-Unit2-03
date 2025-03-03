97775b94f3b5228071d7fbfc3e9574b7763ab18b
Linh Huynh <linh.huynh@mths.ca>
2025-03-03 09:58:00 -0500

    ok

--- code.py
+++ code.py
@@ -1,12 +1,18 @@
+
+#!/usr/bin/env python3
+"""
+Created by: Linh Huynh
+Created on: Mar 2025
+Turn on the LED for one second, then turn off for one second, repeatedly.
+"""
 import board
 import digitalio
 import time
 
-led = digitalio.DigitalInOut(board.LED)
+led = digitalio.DigitalInOut(board.GP5)
 led.direction = digitalio.Direction.OUTPUT
 
 while True:
-    print("Hello, CircuitPython!")
     led.value = True
     time.sleep(1)
     led.value = False
